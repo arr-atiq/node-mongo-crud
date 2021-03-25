8028418d8ad5c635027cc47ac1f650fe05738181
aar-atiq <aar.msdeveloper01@gmail.com>
2021-03-25 23:43:29 +0600

    Save data in database and Read all data

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const bodyParser = require("body-parser");
+
+// const password = "8nzy#u4Uby75XdQ";
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://organicUser:user12345@cluster0.skqkk.mongodb.net/organicProducts?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }))
+
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+})
+
+
+
+client.connect(err => {
+    const collection = client.db("organicProducts").collection("products");
+    app.post("/addProducts", (req, res) => {
+       const product = req.body;
+       collection.insertOne(product)
+       .then(result =>{
+           console.log("data added successfully");
+           res.send('successfully added user!');
+       })
+    })
+});
+
+
+app.listen(3000);
